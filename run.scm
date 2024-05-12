3077a34ff38eb6f81f7ce314f65c094a198368cc
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2024-05-12 13:07:30 -0400

    Added brighten example

--- run.scm
+++ run.scm
@@ -1,8 +1,14 @@
 (load "sdf/manager/load")
 (manage 'new 'generic-interpreter)
-(init)
 
+;; Direct interaction with gimp-submodule
+(load "sdf/generic-interpreter/modules/gimp")
+(call-procedure 'brighten "cat.jpeg" "cat_brighten.jpeg")
+
+;; Main interface
+(init)
 (define cat "cat.jpeg")
 (define dog "dog.jpeg")
 
 (vstack "output:" "catdog.jpeg" cat dog)
+(brighten cat "cat_brightened.jpg")
